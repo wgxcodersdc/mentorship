147b06959b3f8fa390f09fb22143c0c7454cee4c
Ksenia Coulter <kseniacoulter@gmail.com>
2017-12-01 18:56:10 -0500

    made basic table structure

---
+++ script.js
@@ -0,0 +1,25 @@
+
+$(document).ready(function() {
+    $('#example').DataTable( {
+        "data": localData,
+    } );
+} );
+
+var localData = [
+    [
+        "Ksenia Coulter",
+        "Developer",
+        "I'm a web developer at the Washington Post",
+        "Javascript, HTML, CSS, Job Search",
+        "Yes",
+        "Maryland, DC, Online"
+    ],
+    [
+        "Garrett Winters",
+        "UX/UI designer",
+        "Blah blah blah",
+        "UX, UI, Accessibility",
+        "Yes",
+        "Online"
+    ]
+]
